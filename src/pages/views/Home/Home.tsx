b86a2686df7aeb053d7d6775a03fb90b6f3305de
DMv18 <dmena@sistematica.io>
2025-12-23 11:12:44 -0600

    styles al Home

--- src/pages/views/Home/Home.tsx
+++ src/pages/views/Home/Home.tsx
@@ -5,12 +5,14 @@ export default function Home() {
     const { handlerNavigate } = useHookNavigate();
 
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            
-            <Button type="button" onClick={() => handlerNavigate('/process-pay', false)}>
-                Pasarela de Pago
-            </Button>
+        <div className='max-w-md mx-auto p-6'>
+            <h1 className="text-2xl font-bold text-gray-800">Home Page</h1>
+
+            <div className="mt-6 space-y-4">
+                <Button type="button" onClick={() => handlerNavigate('/process-pay', false)}>
+                    Pasarela de Pago
+                </Button>
+            </div>
         </div>
     );
 }
